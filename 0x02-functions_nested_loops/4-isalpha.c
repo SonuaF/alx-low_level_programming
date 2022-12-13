eda3f3726dbcdc5c9421101425dd0f5bb9ae3862
Sonwabiso <fsonwa@gmail.com>
2022-12-13 05:22:34 -0800

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/*
+ * _isalpha - Return 1 if c is a letter
+ *
+ * Return: 1 if c is an alphabet
+ */
+int _isalphabet(int c)
+{
+if (c >= 65 && c <= 122)
+return (1);
+else
+return (0);
+}
