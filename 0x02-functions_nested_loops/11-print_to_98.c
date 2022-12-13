a088e59527be548e8a414c4abf5b5b36829d105b
Sonwabiso <fsonwa@gmail.com>
2022-12-13 07:34:11 -0800

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,16 +7,28 @@
  */
 void print_to_98(int n)
 {
+int i;
 if (n > 98)
 {
-for (n = n; n < 98; n++)
-printf("%d, ", n);
-printf("%d\n", 98);
+for (i = n; i >= 98; i--)
+{
+printf("%d", i);
+if (i != 98)
+{
+printf(", ");
+}
+}
 }
 else
 {
-for (n = n; n > 98; n--)
-printf("%d, ", n);
-printf("%d\n", 98);
+for (i = n; i <= 98; i++)
+{
+printf("%d", i);
+if (i != 98)
+{
+printf(", ");
 }
+}
+}
+printf(", ");
 }
