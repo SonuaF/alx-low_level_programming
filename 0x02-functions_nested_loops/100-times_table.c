134af4834061eec53ce11c54f3832166772a97b9
Sonwabiso <fsonwa@gmail.com>
2022-12-13 23:21:53 -0800

    12. The World looks like a multiplication-table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/*
+ * main - prints a different combination of two digits
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ones = '0';
+int tens = '0';
+for (tens = '0'; tens <= '9'; tens++)
+{
+for (ones = '0'; ones <= '9'; ones++)
+{
+if (!((ones == tens) || (tens > ones)))
+{
+putchar(tens);
+putchar(ones);
+if (!(ones == '9' && tens == '8'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
