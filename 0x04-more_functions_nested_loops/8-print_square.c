a0faa959794ca7710664a6485eb098906c39fa2d
Sonwabiso <fsonwa@gmail.com>
2022-12-14 23:40:38 -0800

    Sunshine in every square inch

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_square - prints last digit
+ *
+ * Return: last digit
+ */
+void print_square(int size)
+{
+int j, i;
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+_putchar ('#');
+_putchar ('\n');
+}
+else
+_putchar ('\n');
+}
