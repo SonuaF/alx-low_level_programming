b1aa88448e3957ef252c9bc3c4c13e2ffda51489
Sonwabiso <fsonwa@gmail.com>
2022-12-14 22:50:56 -0800

    3. The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_numbers - prints numbers
+ *
+ *
+ */
+void print_numbers(void)
+{
+int i;
+for (i = 48; i <= 57; i++)
+_putchar (i);
+_putchar ('\n');
+}
