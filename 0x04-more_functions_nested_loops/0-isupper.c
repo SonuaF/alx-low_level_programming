906e083dc1ba483266eb2a8e9cfb323735d7f2ad
Sonwabiso <fsonwa@gmail.com>
2022-12-14 22:23:12 -0800

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isupper - checks the character entered
+ *
+ * Return: 1 if upper else 0
+ */
+int _isupper(int c)
+{
+if ((c >= 65) && (c <= 90))
+return (1);
+else
+return (0);
+}
