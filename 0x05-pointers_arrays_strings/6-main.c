9880924a95b20033caf21b9d6a382396a174a174
Sonwabiso <fsonwa@gmail.com>
2022-12-19 22:21:34 -0800

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - checks the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+char *str;
+str = "0123456789";
+puts2(str);
+return (0);
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts2 - prints a string
+ * @str - the string printed
+ */
+void puts2(char *str)
+{
+int i, j;
+j = 0;
+while (str[j] != '\0')
+j++;
+for (i = 0; i <= j - 1; i = i + 2)
+_putchar (*(str + i));
+_putchar ('\n');
+}
