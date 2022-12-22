c293209ca3e73db989140e68eb54ec811e87e835
Sonwabiso <fsonwa@gmail.com>
2022-12-22 05:44:28 -0800

    6. Expect the best

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * cap_string - prints words in caps
+ * @c: string
+ * Return: char
+ */
+char *cap_string(char *c)
+{
+int i;
+if (c[0] >= 'a' && c[0] <= 'z')
+c[0] = c[0] - 32;
+for (i = 1; c[i] != '\0'; i++)
+if ((c[i - 1] == ' ' || c[i - 1] == '\t' || c[i - 1] == '\n'
+			|| c[i - 1] == ','
+			|| c[i - 1] == ';' || c[i - 1] == '.' || c[i - 1] == '!'
+			|| c[i - 1] == '?' || c[i - 1] == '"' || c[i - 1] == '('
+			|| c[i - 1] == ')' || c[i - 1] == '{' || c[i - 1] == '}')
+		&& (c[i] > 'a' && c[i] < 'z'))
+c[i] = c[i] - 32;
+return (c);
+}
