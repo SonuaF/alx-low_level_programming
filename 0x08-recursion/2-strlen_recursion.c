547f4bd8774ae98bb122e1b91ce1e5c915a69568
Sonwabiso <fsonwa@gmail.com>
2023-01-04 21:59:25 -0800

    2. Dreams feel real while we're in them

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen_recursion - Returns the length of a string
+ * @s: The string whose length will be returned
+ *
+ * Return: The length of the string
+ */
+int _strlen_recursion(char *s)
+{
+if (*s != '\0')
+return (1 + _strlen_recursion(s + 1));
+return (0);
+}
