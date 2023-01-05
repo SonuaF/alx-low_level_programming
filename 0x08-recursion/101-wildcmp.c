514322eae400e31235f381da9493669e281ef30f
Sonwabiso <fsonwa@gmail.com>
2023-01-04 23:23:16 -0800

    8. Inception.

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * wildcmp - Compares strings
+ * @s1: pointer
+ * @s2: pointer
+ * Return: 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+if (*s1 == '\0')
+{
+if (*s2 != '\0' && *s2 == '*')
+{
+return (wildcmp(s1, s2 + 1));
+}
+return (*s2 == '\0');
+}
+if (*s2 == '*')
+{
+return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+}
+else if (*s1 == *s2)
+{
+return (wildcmp(s1 + 1, s2 + 1));
+}
+return (0);
+}
