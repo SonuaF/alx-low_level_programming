8f2512e16dd9f2d5c045cd54ecbd5a4b2cb7d7e7
Sonwabiso <fsonwa@gmail.com>
2023-01-25 13:18:37 -0800

    0. What my name

--- 0x10-variadic_functions/0x0F-function_pointers/0-print_name.c
+++ 0x10-variadic_functions/0x0F-function_pointers/0-print_name.c
@@ -11,7 +11,6 @@
  **/
 void print_name(char *name, void (*f)(char *))
 {
-if (!name || !f)
-return;
+if (name != NULL && f != NULL)
 f(name);
 }
--- 0x10-variadic_functions/0x0F-function_pointers/function_pointers.h
+++ 0x10-variadic_functions/0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
 #ifndef FUNCTION_POINTERS_H
 
 #define FUNCTION_POINTERS_H
-
+#include <stdlib.h>
 
 
 void print_name(char *name, void (*f)(char *));
