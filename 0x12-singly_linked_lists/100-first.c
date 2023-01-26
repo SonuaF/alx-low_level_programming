11d525a548ac678418ad0d59f007317ec40c7331
Sonwabiso <fsonwa@gmail.com>
2023-01-26 01:31:44 -0800

    5. The hare

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+#include <stdio.h>
+
+/**
+ * first - print the string before the main is executed
+ **/
+
+void __attribute__ ((constructor)) first()
+
+{
+
+	printf("You're beat! and yet, you must allow");
+
+	printf(",\nI bore my house upon my back!\n");
+
+}
