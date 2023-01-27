31efc23178e43f571a6b68efba2f71f7b3c3ffe0
Sonwabiso <fsonwa@gmail.com>
2023-01-26 23:21:23 -0800

    2. File name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+/**
+ * main - Prints the name of the file
+ *
+ * Return: Always 0
+ **/
+
+int main(void)
+
+{
+
+	printf("%s\n", __FILE__);
+
+	return (0);
+
+}
