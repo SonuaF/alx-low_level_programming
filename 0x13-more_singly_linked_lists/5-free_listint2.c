d6ff3a506f51f063b86cb24cc5c8b85346f87bf1
Sonwabiso <fsonwa@gmail.com>
2023-01-30 09:36:41 -0800

    5. Free list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,33 +6,16 @@
  * free_listint2 - Free all the elements of a linked list
  * @head: A pointer to first node of list
  **/
-
-
-
 void free_listint2(listint_t **head)
-
 {
-
-	listint_t *t;
-
-
-
-	if (head == NULL)
-
-		return;
-
-	t = *head;
-
-	while (t != NULL)
-
-	{
-
-		free(t);
-
-		t = t->next;
-
-	}
-
-	*head = NULL;
-
+listint_t *temp;
+if (head == NULL)
+return;
+while (*head)
+{
+temp = (*head)->next;
+free(*head);
+*head = temp;
+}
+*head = NULL;
 }
