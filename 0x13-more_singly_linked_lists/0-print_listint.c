a347368b12d25003a469483a33b063dbdff9eb03
Sonwabiso <fsonwa@gmail.com>
2023-01-30 13:26:06 -0800

    0. print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,20 +1,35 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
+
 /**
- * print_listint - print nodes and return nbr of nodes
+ * print_listint -print nodes and return nbr of nodes
  * @h: first node of the list
  *
  * Return: number of nodes.
  **/
 
 size_t print_listint(const listint_t *h)
+
 {
-size_t nodenum = 0;
-while (h)
-{
-printf("%d\n", h->n);
-nodenum++;
-h = h->next;
-}
-return (nodenum);
+
+	int l = 0;
+
+	if (h == NULL)
+
+		return (0);
+
+	while (h)
+
+	{
+
+		printf("%d\n", h->n);
+
+		l++;
+
+		h = h->next;
+
+	}
+
+	return (l);
+
 }
