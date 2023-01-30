de95a9053c9b664a08fc852c072c4663145c593a
Sonwabiso <fsonwa@gmail.com>
2023-01-30 09:24:20 -0800

    4. Free list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -5,30 +5,14 @@
 /**
  * free_listint - Free linked list
  * @head: The first element
- **/
-
-
-
+ */
 void free_listint(listint_t *head)
-
 {
-
-	listint_t *t;
-
-
-
-	t = head;
-
-	while (head != NULL)
-
-	{
-
-		head = head->next;
-
-		free(head);
-
-	}
-
-	free(t);
-
+listint_t *temp;
+while (head)
+{
+temp = head->next;
+free(head);
+head = temp;
+}
 }
