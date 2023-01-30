13ebe16ddad68f6e78838058b4837c1fd03ff065
Sonwabiso <fsonwa@gmail.com>
2023-01-30 13:12:53 -0800

    9. Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,65 +11,33 @@
  * Return: node
  **/
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
-
 {
-
-	listint_t *p, *new;
-
-	unsigned int i = 0;
-
-
-
-	p = *head;
-
-	if (head == NULL && idx != 0)
-
-		return (NULL);
-
-	new = malloc(sizeof(listint_t));
-
-	if (new == NULL)
-
-		return (NULL);
-
-	new->n = n;
-
-	if (idx == 0)
-
-	{
-
-		new->next = *head;
-
-		*head = new;
-
-		return (new);
-
-	}
-
-	while (i < idx - 1)
-
-	{
-
-		p = p->next;
-
-		if (p == NULL)
-
-		{
-
-			free(new);
-
-			return (NULL);
-
-		}
-
-		i++;
-
-	}
-
-	new->next = p->next;
-
-	p->next = new;
-
-	return (new);
-
+listint_t *node, *temp = *head;
+size_t i = 0;
+node = malloc(sizeof(listint_t));
+if (!node)
+return (NULL);
+node->n = n;
+node->next = NULL;
+if (!*head && !idx)
+{
+*head = node;
+return (node);
+}
+else if (!*head && idx)
+return (NULL);
+else if (!idx)
+{
+node->next = temp;
+*head = node;
+return (node);
+}
+while (i < (idx - 1))
+{
+temp = temp->next;
+i++;
+}
+node->next = temp->next;
+temp->next = node;
+return (node);
 }
